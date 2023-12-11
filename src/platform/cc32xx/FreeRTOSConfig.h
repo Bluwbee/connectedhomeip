951589e525503d3ece1c64f7a08fc5ab10b24347
Tony Cave <tony@bluwbee.com>
2023-12-11 21:20:41 +0000

    KVS updates

--- src/platform/cc32xx/FreeRTOSConfig.h
+++ src/platform/cc32xx/FreeRTOSConfig.h
@@ -55,7 +55,8 @@ supported, or if both static and dynamic allocation are supported. */
 #define configMINIMAL_STACK_SIZE ((unsigned short) 256) // changed from 128
 #define configMAX_TASK_NAME_LEN (12)
 
-#define configTOTAL_HEAP_SIZE ((size_t) (137160))
+//#define configTOTAL_HEAP_SIZE ((size_t) 148200)
+#define configTOTAL_HEAP_SIZE ((size_t) 139904)
 
 /* Idle task stack size in words */
 #define configIDLE_TASK_STACK_DEPTH (configMINIMAL_STACK_SIZE * 2)
@@ -88,6 +89,7 @@ supported, or if both static and dynamic allocation are supported. */
             ;                                                                                                                      \
     }
 #define configQUEUE_REGISTRY_SIZE 0
+#define configRECORD_STACK_HIGH_ADDRESS 1
 
 /*
  *  Minimum number of full tick periods of idle time required to run Power
